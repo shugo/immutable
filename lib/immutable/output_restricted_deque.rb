3020ee6081efe7c97889efa974ae15008b13c232
Shugo Maeda <shugo@ruby-lang.org>
2012-04-26 15:48:57 +0900

    add Immutable::OutputRestrictedDeque.

---
+++ lib/immutable/output_restricted_deque.rb
@@ -0,0 +1,18 @@
+require "immutable/queue"
+
+module Immutable
+  # +Immutable::OutputRestrictedDeque+ is an implementation of
+  # output-restricted deques described in "Purely Functional Data
+  # Structures" by Chris Okasaki.
+  class OutputRestrictedDeque < Queue
+    # Adds a new element at the head of +self+.
+    #
+    # @param [Object] x the element to add.
+    # @return [Queue] a new queue.
+    def cons(x)
+      self.class.new(Stream.cons(->{x}, ->{@front}), @rear,
+                     Stream.cons(->{x}, ->{@schedule}))
+    end
+    alias prepend cons
+  end
+end
