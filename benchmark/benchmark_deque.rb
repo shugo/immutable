82cfb3e1f372da8d1f0b90671ae4d43b4c9d23b8
Shugo Maeda <shugo@ruby-lang.org>
2012-04-26 17:35:31 +0900

    add benchmark for last, init, and cons.

--- benchmark/benchmark_deque.rb
+++ benchmark/benchmark_deque.rb
@@ -5,21 +5,39 @@ require 'immutable/map'
 TIMES = 100000
 key_size = 10
 
-def snoc(deque)
-  TIMES.times do
-    deque.snoc(0)
-  end
-end
-
 def head(deque)
   TIMES.times do
     deque.head
   end
 end
 
+def last(deque)
+  TIMES.times do
+    deque.last
+  end
+end
+
 def tail(deque)
   TIMES.times do
     deque.tail
+  end
+end
+
+def init(deque)
+  TIMES.times do
+    deque.init
+  end
+end
+
+def cons(deque)
+  TIMES.times do
+    deque.cons(0)
+  end
+end
+
+def snoc(deque)
+  TIMES.times do
+    deque.snoc(0)
   end
 end
 
@@ -33,7 +51,7 @@ Benchmark.bmbm do |bm|
   deques = [100, 1000, 10000].inject(Immutable::Map.empty) { |m, n|
     m.insert(n, (1..n).inject(Immutable::Deque.empty, &:snoc))
   }
-  for method in [:snoc, :head, :tail]
+  for method in [:head, :last, :tail, :init, :cons, :snoc]
     for num, deque in deques
       run(bm, deque, num, method)
     end
