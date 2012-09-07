f31bcea6ac9b9fe911336d943ae3387bced57c1b
Shugo Maeda <shugo@ruby-lang.org>
2012-09-07 18:21:19 +0900

    enable tail call optimization.

--- lib/immutable.rb
+++ lib/immutable.rb
@@ -2,10 +2,19 @@
 module Immutable
 end
 
-require_relative "immutable/list"
-require_relative "immutable/map"
-require_relative "immutable/promise"
-require_relative "immutable/stream"
-require_relative "immutable/queue"
-require_relative "immutable/output_restricted_deque"
-require_relative "immutable/deque"
+old_compile_option = RubyVM::InstructionSequence.compile_option
+RubyVM::InstructionSequence.compile_option = {
+  :tailcall_optimization => true,
+  :trace_instruction => false
+}
+begin
+  require_relative "immutable/list"
+  require_relative "immutable/map"
+  require_relative "immutable/promise"
+  require_relative "immutable/stream"
+  require_relative "immutable/queue"
+  require_relative "immutable/output_restricted_deque"
+  require_relative "immutable/deque"
+ensure
+  RubyVM::InstructionSequence.compile_option = old_compile_option
+end
